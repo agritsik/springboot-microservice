3e3c6cf25d73b96eb8abf2d06f1215cffa0410f9
andrey <a.gritsik@gmail.com>
2016-03-26 11:42:51 +0200

    add more tests

--- src/test/java/com/agritsik/samples/blog/boundary/PostResourceTest.java
+++ src/test/java/com/agritsik/samples/blog/boundary/PostResourceTest.java
@@ -8,24 +8,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PostResourceTest {
+
+    public static final String POST_RESOURCE = "/resources/posts/";
+    public static final Long POST_ID = 1L;
 
     private MockMvc mvc;
 
@@ -38,7 +41,7 @@ public class PostResourceTest {
     @Before
     public void setUp() throws Exception {
         this.mvc = standaloneSetup(postResource)
-                .alwaysDo(print()) // always print the result
+                // .alwaysDo(print()) // always print the result
                 .build();
     }
 
@@ -50,7 +53,7 @@ public class PostResourceTest {
         String postJson = new ObjectMapper().writeValueAsString(new Post("1st"));
 
         // act && assert
-        this.mvc.perform(post("/resources/posts")
+        this.mvc.perform(post(POST_RESOURCE)
                 .content(postJson).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(content().string(""));
@@ -64,11 +67,53 @@ public class PostResourceTest {
         when(postAction.find(anyLong())).thenReturn(new Post("2nd"));
 
         // act && assert
-        this.mvc.perform(get("/resources/posts/1"))
+        this.mvc.perform(get(POST_RESOURCE + POST_ID))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().string(containsString("2nd")));
 
         verify(postAction).find(anyLong());
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+
+        // arrange
+        Page mockedPage = Mockito.mock(Page.class, CALLS_REAL_METHODS);
+        when(postAction.find(anyInt(), anyInt())).thenReturn(mockedPage);
+
+        // act && assert
+        this.mvc.perform(get(POST_RESOURCE + "?start={start}&maxResult={maxResult}", 2, 10))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+        // arrange
+        String postJson = new ObjectMapper().writeValueAsString(new Post("3rd"));
+
+        // act && assert
+        this.mvc.perform(put(POST_RESOURCE + POST_ID)
+                .content(postJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(""));
+
+        verify(postAction).update(any(Post.class));
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+        // act && verify
+        this.mvc.perform(delete(POST_RESOURCE + POST_ID))
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(""));
+
+        verify(postAction).delete(anyLong());
+
     }
 }
--- src/test/java/com/agritsik/samples/blog/control/PostActionTest.java
+++ src/test/java/com/agritsik/samples/blog/control/PostActionTest.java
@@ -9,8 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.domain.PageRequest;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PostActionTest {
@@ -40,5 +43,59 @@ public class PostActionTest {
 
     }
 
+    @Test
+    public void testFind() throws Exception {
 
+        // arrange
+        Long postId = 1L;
+
+        // act
+        postAction.find(postId);
+
+        // assert
+        verify(postRepository).findOne(postId);
+        verifyZeroInteractions(rabbitTemplate);
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+        // act
+        postAction.find(5, 10);
+
+        // assert
+        verify(postRepository).findAll(any(PageRequest.class));
+        verifyZeroInteractions(rabbitTemplate);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+        // arrange
+        Post post = new Post("1nd");
+
+        // act
+        postAction.update(post);
+
+        // assert
+        verify(postRepository).save(post);
+        verifyZeroInteractions(rabbitTemplate);
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+        // arrange
+        long postId = 1;
+
+        // act
+        postAction.delete(postId);
+
+        // assert
+        verify(postRepository).delete(postId);
+        verifyZeroInteractions(rabbitTemplate);
+
+    }
 }
